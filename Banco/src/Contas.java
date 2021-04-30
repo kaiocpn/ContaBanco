1f09cb5e8e364fa718ca9662774ebb166340e263
CASA <CASA@DESKTOP-HQ1FO1N>
2021-04-30 20:18:50 -0300

    Banco

---
+++ Banco/src/Contas.java
@@ -0,0 +1,31 @@
+
+public class Contas {
+
+	public static void main(String[] args) {
+		ContaBanco p1 = new ContaBanco();
+		p1.setNumConta(1111);
+		p1.setDono("Kaio");
+		p1.abrirConta("CC");
+		
+		
+		
+		ContaBanco p2 = new ContaBanco();
+		p2.setNumConta(2222);
+		p2.setDono("Maria");
+		p2.abrirConta("CP");
+		
+		p1.depositar(300);
+		p2.depositar(500);
+		p2.sacar(100);
+		
+	
+		
+		p1.estadoAtual();
+		p2.estadoAtual();
+		
+		
+		
+
+	}
+
+}
